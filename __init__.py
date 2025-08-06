074c995437597abeec58d18639bab2fffb7962d5
chris-r-uol <78163635+chris-r-uol@users.noreply.github.com>
2025-08-06 14:33:49 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,8 +8,8 @@ from .quantile_multi_comparison import quantile_comparison_plot, cullen_and_frey
 from .empirical_plots import empirical_cdf_plot, empirical_density_plot
 
 __version__ = "0.1.0"
-__author__ = "Chris Russell"
-__email__ = "your.email@leeds.ac.uk"
+__author__ = "Chris Rushton"
+__email__ = "c.e.rushton@leeds.ac.uk"
 
 __all__ = [
     "quantile_comparison_plot", 
